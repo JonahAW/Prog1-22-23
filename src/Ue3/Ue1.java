59dc3e8a936504b48f4254403d9f984a610549ec
WichmannJ <jonahake.wichmann@gmail.com>
2022-11-29 15:51:11 +0100

    Nächster Stand

--- src/Ue3/Ue1.java
+++ src/Ue3/Ue1.java
@@ -1,3 +1,5 @@
+package Ue3;
+
 import java.util.Scanner;
 
 public class Ue1 {
@@ -6,6 +8,7 @@ public class Ue1 {
     public static int w1;
     public static int w2;
     public static int erg;
+    static boolean berechne;
     static Scanner eingabe = new Scanner(System.in);
     public static void einlesen() {
         System.out.println("Bitte geben Sie ein Operant (a=Addition, s=Subtration, m=Multiplikation & d=Division) ein.");
@@ -19,26 +22,35 @@ public class Ue1 {
     public static boolean berechne() {
         if (o == "a") {
             erg = w1 + w2;
+            return true;
         } else if (o == "s") {
             erg = w1 - w2;
+            return true;
         } else if (o == "m") {
             erg = w1 * w2;
+            return true;
         } else if (o == "d") {
             erg = w1 / w2;
+            return true;
         } else if (o == "") {
             System.out.println("Bitte geben Sie ein Operanten ein.");
+            return true;
         }
-        return true;
+        return false;
     }
 
     public static void ausgeben() {
-        System.out.println("Ihre Berechnung war:"+ w1 + o + w2);
-        System.out.println("Ihr Ergebnis lautet:"+ erg);
+
+        if (berechne == true) {
+            System.out.println("Ihre Berechnung war:" + w1 + o + w2);
+            System.out.println("Ihr Ergebnis lautet:" + erg);
+        }
     }
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
         einlesen();
+        berechne();
         ausgeben();
     }
 }
--- src/Ue3/Ue2.java
+++ src/Ue3/Ue2.java
@@ -1,3 +1,7 @@
+package Ue3;
+
+import
+
 public class Ue2 {
     public static void main(String[] args) {
 
